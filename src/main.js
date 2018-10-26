ab72b2f266fdfa28856c99717506bd0e35cee645
dyh <987520247@qq.com>
2018-10-26 14:53:08 +0800

    fix  页面在浏览器缩放和横屏

--- src/main.js
+++ src/main.js
@@ -74,6 +74,29 @@ router.beforeEach((to, from, next) => {
   next()
 })
 
+// ios禁止页面缩放
+window.onload = function() {
+  // 阻止双击放大
+  var lastTouchEnd = 0;
+  document.addEventListener('touchstart', function(event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  });
+  document.addEventListener('touchend', function(event) {
+    var now = (new Date()).getTime();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  }, false);
+
+  // 阻止双指放大
+  document.addEventListener('gesturestart', function(event) {
+    event.preventDefault();
+  });
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
